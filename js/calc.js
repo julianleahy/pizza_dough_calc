15786495a42477ec5489d615148ed55f2aaa757a
julianleahy <julianleahynz@gmail.com>
2018-07-06 21:29:25 +1200

    add an init function and move event listeners to it

--- js/calc.js
+++ js/calc.js
@@ -29,23 +29,32 @@ let UIController = (() => {
 // GLOBAL APP CONTROLLER
 let appController = ((calcCtrl, UICtrl) => {
 
-  let clickEvent = () => {
-    let { numPizzas, sizePizza } = UICtrl.getInputData();
-    console.log(`${numPizzas} ${sizePizza} pizza`);
-  }
-
-  // 1. Listen for click event on pizza btn
-  document.getElementById('calculate').addEventListener('click', clickEvent)
-  // 1b. Handle return / enter
-  document.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') clickEvent();
-  });
 
   // 2: Get field input data
+  let clickEvent = () => {
+    // Destructure returned object
+    const { numPizzas, sizePizza } = UICtrl.getInputData();
+    console.log(`${numPizzas} ${sizePizza} pizzas`);
+  }
+
+  // add an init function
+  return {
+    init: () => {
+      // 1. Listen for click event on pizza btn
+      document.getElementById('calculate').addEventListener('click', clickEvent)
+      // 1b. Handle return / enter
+      document.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') clickEvent();
+      });
+    }
+  }
+
 
   // 3: Calculate ingredient amounts
 
   // 4: Update UI
 
 })(calcController, UIController);
+
+appController.init();
 
