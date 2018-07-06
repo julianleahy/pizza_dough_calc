5d1bec74bfed3891fc730c9d602b8969722aa4d3
julianleahy <julianleahynz@gmail.com>
2018-07-06 20:55:19 +1200

    add controllers structure

---
+++ js/calc.js
@@ -0,0 +1,30 @@
+
+// CALCULATE AMOUNTS CONTROLLER
+let calcController = (() => {
+
+  // Calculating Amounts
+
+})();
+
+// UI CONTROLLER
+let UIController = (() => {
+
+  // UI Stuff
+
+
+
+})();
+
+// GLOBAL APP CONTROLLER
+let appController = ((calcCtrl, UICtrl) => {
+
+  // 1. Listen for click event on pizza btn
+
+  // 2: Get field input data
+
+  // 3: Calculate ingredient amounts
+
+  // 4: Update UI
+
+})(calcController, UIController);
+
