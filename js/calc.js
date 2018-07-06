176be3fb01050a6d9695eefe5b411f79b9fb0e8c
julianleahy <julianleahynz@gmail.com>
2018-07-06 21:08:44 +1200

    add event listenter for click or return pressed

--- js/calc.js
+++ js/calc.js
@@ -18,7 +18,16 @@ let UIController = (() => {
 // GLOBAL APP CONTROLLER
 let appController = ((calcCtrl, UICtrl) => {
 
+  let clickEvent = () => {
+    console.log('OK');
+  }
+
   // 1. Listen for click event on pizza btn
+  document.getElementById('calculate').addEventListener('click', clickEvent)
+  // 1b. Handle return / enter
+  document.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') clickEvent();
+  });
 
   // 2: Get field input data
 
