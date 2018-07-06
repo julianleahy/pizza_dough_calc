a8aeefd781e4590d80023562f473a14801ae80f3
julianleahy <julianleahynz@gmail.com>
2018-07-06 22:25:18 +1200

    calc ingredient amounts and retrun to app controller

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,30 @@
 // CALCULATE AMOUNTS CONTROLLER
 let calcController = (() => {
 
-  // Calculating Amounts
+  // Percentage of overall weight
+  const percentage = {
+    flour: 0.625,
+    water: 0.356,
+    salt: 0.0178,
+    yeast: 0.0012
+  }
+
+  return {
+    calculate: (num, size) => {
+      let weight = num * size;
+
+      // Return calculated ingredients based on weight
+      // Round up all ingredients except yeast as irrelavant
+      return {
+        flour: Math.floor(percentage.flour * weight),
+        water: Math.floor(percentage.water * weight),
+        salt: Math.floor(percentage.salt * weight),
+        yeast: percentage.yeast * weight,
+        numPizzas: num,
+        size: size
+      }
+    }
+  }
 
 })();
 
@@ -30,7 +53,9 @@ let appController = ((calcCtrl, UICtrl) => {
   let clickEvent = () => {
     // Destructure returned object
     const { numPizzas, sizePizza } = UICtrl.getInputData();
-    console.log(`${numPizzas} ${sizePizza} pizzas`);
+    const ingredients = calcCtrl.calculate(numPizzas, sizePizza);
+
+    console.log(ingredients);
   }
 
   // add an init function
